550070d3c69d736dd332a39906b0c581160ede1e
Francisco Lopez <frank_lopez@Franciscos-M3.local>
2024-10-23 10:37:02 -0700

    added some design and components that will be part of the final product.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
 const collapsibles = document.querySelectorAll(".collapsible");
+
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible-expanded");
   })
 );
+
+// show the Tabs page expanded
+
+const tab = document.getElementById("tabs");
+
+
+tab.addEventListener("click", function() {
+  if ( tab.classList.contains("collapsible-expanded") ) {
+    document.getElementById("tabs-expand").style.display = "block";
+  } else {
+    document.getElementById("tabs-expand").style.display = "none";
+  }
+
+})
+
+
+// Show the Shared Tabs page 
+
+const share = document.getElementById("shared");
+
+share.addEventListener("click", function() {
+  if ( share.classList.contains("collapsible-expanded") ) {
+    document.getElementById("shared-expand").style.display = "block";
+  } else {
+    document.getElementById("shared-expand").style.display = "none";
+  }
+
+})
